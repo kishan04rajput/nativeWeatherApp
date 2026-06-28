db384e640862e013e23cc7565c904ddf995400b5
kishan04rajput <kishan9rajput@gmail.com>
2026-06-28 19:58:16 +0530

    chore: add expo-dev-client, downgrade tailwindcss, enable tsconfigPaths, and update path aliases

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,9 +3,6 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-slate-100">
-      <Text className="text-lg font-bold text-slate-800">
-        Tailwind CSS is now setup!
-      </Text>
       <Text className="text-sm text-slate-600 mt-2">
         Edit src/app/index.tsx to start building.
       </Text>
