6fe04b89516ba4a38116b8fb73278fef68e19e6c
Kishan Rajput <44379153+kishan04rajput@users.noreply.github.com>
2026-06-28 20:05:17 +0530

    Initial setup (#1)
    Whats New?
    - setup of react native expo 56
    - Added native wind and tail wind
    - Added expo-dev-client

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-100">
+      <Text className="text-sm text-slate-600 mt-2">
+        Edit src/app/index.tsx to start building.
+      </Text>
+    </View>
+  );
+}
+
